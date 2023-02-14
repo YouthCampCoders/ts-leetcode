7cf6966e87496dae35a772c2335c4803359033cd
chin <1277180540@qq.com>
2023-02-14 23:24:20 +0800

    chore: 处理报错

--- qhy/tree/199.二叉树的右视图.ts
+++ qhy/tree/199.二叉树的右视图.ts
@@ -19,6 +19,18 @@
  * }
  */
 
+export class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.left = left === undefined ? null : left
+    this.right = right === undefined ? null : right
+  }
+}
+
 // 层序遍历
 function rightSideView(root: TreeNode | null): number[] {
   if (!root) return []
